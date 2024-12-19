c4a905ff147e72dc4c38f30e6c20d035a872e31d
dejan966 <ultimate24208@gmail.com>
2024-12-19 17:07:10 +0100

    migrations

---
+++ database/migrations/2024_12_19_154522_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('arrival_date');
+            $table->dateTime('departure_date');
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->string('name_surname');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
