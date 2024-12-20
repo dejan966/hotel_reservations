f5aee98c689380825b5ab06491856565ae8c7324
dejan966 <ultimate24208@gmail.com>
2024-12-20 17:44:10 +0100

    Display rooms in table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
-
+@php
+$rooms = App\Models\Room::all();
+@endphp
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -8,13 +10,26 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <table class="table">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Price per night(€)</th>
+                            <th>Short description</th>
+                            <th>Long description</th>
+                            <th>Edit</th>
+                        </tr>
+                        <tr>
+                            @foreach ($rooms as $room)
+                                <td>{{ $room->id }}</td>
+                                <td>{{ $room->name }}</td>
+                                <td>{{ $room->price }}</td>
+                                <td>{{ $room->short_description }}</td>
+                                <td>{{ $room->long_description }}</td>
+                                <td>edit</td>
+                            @endforeach
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
