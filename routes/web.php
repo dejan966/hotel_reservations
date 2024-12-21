67e4e58cbbbcfead70749b7c75d244807528ddb6
dejan966 <ultimate24208@gmail.com>
2024-12-21 16:18:34 +0100

    Auth

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations');
 Route::post('/reservations', [ReservationController::class, 'createReservartions'])->name('reservations.create');
-Route::get('/rooms/edit/{id}', [RoomController::class, 'openEdit'])->name('room.edit');
-Route::post('/rooms/edit/{id}', [RoomController::class, 'updateRoom'])->name('room.update');
+
+Route::middleware('auth')->group(function() {
+    Route::get('/rooms/edit/{id}', [RoomController::class, 'openEdit'])->name('room.edit');
+    Route::post('/rooms/edit/{id}', [RoomController::class, 'updateRoom'])->name('room.update');
+});
